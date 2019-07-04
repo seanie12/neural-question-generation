1fbf74983a9cecf5add0657f5a604cac0c25cccc
Seanie Lee <lsnfamily02@naver.com>
2019-07-04 15:59:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-from trainer import QGTrainer, DualTrainer, QATrainer
+from trainer import Trainer
 from infenrence import BeamSearcher
 import config
 
 
 def main():
     if config.train:
-        # trainer = DualTrainer(config.qa_path, config.ca2q_path, config.c2q_path, config.c2a_path)
-        # trainer = QGTrainer()
-        trainer = QATrainer()
+        trainer = Trainer()
         trainer.train()
     else:
-        print("start decoding")
         beamsearcher = BeamSearcher(config.model_path, config.output_dir)
         beamsearcher.decode()
 
